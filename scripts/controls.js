66d255b18be638e728dbe1c097fc9011edf569cd
liuus <210084@mt.feitian.edu>
2021-02-18 12:46:03 -0500

    greyed out unusable Sentinel satellite data menu

--- scripts/controls.js
+++ scripts/controls.js
@@ -471,10 +471,6 @@ define([
         thirdL = thirdL.replace('()', '');
         let allToggle = thirdL + '-alltoggle';
 
-        let panelDefault3 = document.createElement("div");
-        panelDefault3.id = thirdL;
-        panelDefault3.className = "Menu panel panel-info " + thirdL + " " + secondL + " " + firstL + "-" + secondL + " " + firstL + "-" + secondL + "-" + thirdL;
-
         let panelHeading3 = document.createElement("div");
         panelHeading3.className = "panel-heading " + firstL + "-" + secondL + "-" + thirdL;
 
@@ -482,12 +478,24 @@ define([
         panelTitle3.className = "panel-title " + firstL + "-" + secondL + "-" + thirdL;
 
         let collapsed3 = document.createElement("a");
-        collapsed3.className = "collapsed";
-        if (thirdL !== 'Country' && thirdL != 'Agriculture' && thirdL != 'False Color (Urban)' && thirdL != 'False Color (Vegetation)' && thirdL != 'Geology' && thirdL != 'Natural Color (True Color)') {
+
+        let panelDefault3 = document.createElement("div");
+        panelDefault3.id = thirdL;
+
+        if (thirdL !== 'Country' && thirdL !== 'Agriculture' && ThirdL !== 'False Color (Urban)' && ThirdL !== 'False Color (Vegetation)' && thirdL !== 'Geology' && ThirdL !== 'Natural Color (True Color)') {
         // if (thirdL !== 'Country') {
+            collapsed3.className = "collapsed";
             collapsed3.href = "#" + firstL + "-" + secondL + "-" + thirdL;
             collapsed3.setAttribute("data-toggle", "collapse");
             collapsed3.setAttribute("data-parent", "#nested");
+            panelDefault3.className = "Menu panel panel-info " + thirdL + " " + secondL + " " + firstL + "-" + secondL + " " + firstL + "-" + secondL + "-" + thirdL;
+        } else if (SecondL === "Sentinel Satellite Data") {
+        // } else if (thirdL == 'Agriculture' && thirdL == 'False Color (Urban)' && thirdL == 'False Color (Vegetation)' && thirdL == 'Geology' && thirdL == 'Natural Color (True Color)') {
+            collapsed3.className = "collapsed disabled-link";
+            panelDefault3.className = "Menu panel disabled-menu " + thirdL + " " + secondL + " " + firstL + "-" + secondL + " " + firstL + "-" + secondL + "-" + thirdL;
+        } else {
+            collapsed3.className = "collapsed";
+            panelDefault3.className = "Menu panel panel-info " + thirdL + " " + secondL + " " + firstL + "-" + secondL + " " + firstL + "-" + secondL + "-" + thirdL;
         }
         collapsed3.id = firstL + "-" + secondL + "-" + thirdL + '-a';
 
@@ -528,6 +536,8 @@ define([
         // if (thirdL !== 'Country') {
             panelDefault3.appendChild(nested1c1);
             nested1c1.appendChild(panelBody4);
+        } else if (SecondL === "Sentinel Satellite Data") {
+
         }
         //// checkboxA.appendChild(checkboxAt);
         //// checkboxH4.appendChild(checkboxA);
